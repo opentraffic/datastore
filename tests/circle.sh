153dc09f353d1aedea02c3d0c68e9c41e87f015a
Kevin Kreiser <kevinkreiser@gmail.com>
2017-02-06 12:32:56 -0500

    more waiting and retrying

--- tests/circle.sh
+++ tests/circle.sh
@@ -7,7 +7,7 @@ docker run \
   --name datastore-postgres \
   -d postgres:9.6.1
 
-sleep 15
+sleep 30
 
 echo "Starting the datastore container..."
 docker run \
@@ -18,7 +18,7 @@ docker run \
   -v ${PWD}/data:/data \
   opentraffic/datastore
 
-sleep 15
+sleep 30
 
 # basic json validation
 echo "Validating json request data..."
@@ -26,6 +26,6 @@ jq "." tests/datastore_request.json >/dev/null
 
 # test the generated data against the service
 echo "Running the test data through the datastore service..."
-curl -s --data tests/datastore_request.json localhost:8003/store?
+curl -s --max-time 25 --retry 5 --retry-delay 5 --data tests/datastore_request.json localhost:8003/store?
 
 echo "Done!"
