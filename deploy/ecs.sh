811965addf9060f33ad758097f52c86b633e483a
Grant Heffernan <heffergm@gmail.com>
2017-02-09 14:56:33 -0500

    increase stale deployment wait time to 5 minutes

--- deploy/ecs.sh
+++ deploy/ecs.sh
@@ -42,7 +42,7 @@ deploy_cluster() {
             $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
       echo "Waiting for stale deployments:"
       echo "$stale"
-      sleep 5
+      sleep 10
     else
       echo "Deployed!"
       return 0
