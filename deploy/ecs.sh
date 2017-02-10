3d2ab9204890234349e682cc4d113bcaabb47097
Grant Heffernan <heffergm@gmail.com>
2017-02-10 06:56:35 -0500

    fix missing env

--- deploy/ecs.sh
+++ deploy/ecs.sh
@@ -94,7 +94,7 @@ make_task_def(){
 push_ecr_image(){
   eval $(aws ecr get-login --region us-east-1)
   docker tag datastore:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/opentraffic/datastore-$ENV:$CIRCLE_SHA1
-  docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/opentraffic/datastore:$CIRCLE_SHA1
+  docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/opentraffic/datastore-$ENV:$CIRCLE_SHA1
 }
 
 register_definition() {
