460c0e4680310135f274a577b9864555f64f178e
Grant Heffernan <heffergm@gmail.com>
2017-06-22 14:10:28 -0400

    lint

--- tests/circle.sh
+++ tests/circle.sh
@@ -9,7 +9,7 @@ docker run \
   -v ${PWD}/tests/work-data:/work \
   -v ${PWD}/output:/output \
   datastore:latest \
-  datastore-histogram-tile-writer -b 410562 -t 17120 -v -f /output/output.fb ./1478023200_1478026799/0/2140/*
+  datastore-histogram-tile-writer -b $((1478023200/3600)) -t $(((2140 << 3) | 0)) -v -f /output/output.fb /work/1478023200_1478026799/0/2140/*
 
 ls -l output
 echo "Done!"
