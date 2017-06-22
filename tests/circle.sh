b43f37877f6e855482146f305eca30f554202fb1
Grant Heffernan <heffergm@gmail.com>
2017-06-22 13:38:55 -0400

    operate on data

--- tests/circle.sh
+++ tests/circle.sh
@@ -6,6 +6,6 @@ docker run \
   --name datastore \
   -v ${PWD}/tests/work-data:/work \
   datastore:latest \
-  datastore-histogram-tile-writer -f /output/flatbuffer_file -o /output/orc_file ./*
+  datastore-histogram-tile-writer -b $((1478023200/3600)) -t $(((2140 << 3) | 0)) -v -f /output/output.fb ./1478023200_1478026799/0/2140/*
 
 echo "Done!"
