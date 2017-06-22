bad63d15bedf3d5901d787a8fee287b06d18e24d
Grant Heffernan <heffergm@gmail.com>
2017-06-22 14:15:02 -0400

    test output file

--- tests/circle.sh
+++ tests/circle.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+output_file=/output/output.fb
+
 mkdir output
 
 echo "Starting the datastore container..."
@@ -9,7 +11,13 @@ docker run \
   -v ${PWD}/tests/work-data:/work \
   -v ${PWD}/output:/output \
   datastore:latest \
-  datastore-histogram-tile-writer -b $((1478023200/3600)) -t $(((2140 << 3) | 0)) -v -f /output/output.fb /work/1478023200_1478026799/0/2140/*
+  datastore-histogram-tile-writer -b $((1478023200/3600)) -t $(((2140 << 3) | 0)) -v -f ${output_file} /work/1478023200_1478026799/0/2140/*
 
-ls -l output
-echo "Done!"
+if [ -f ${output_file} ]; then
+  echo "Success!"
+  ls -l ${output_file}
+  exit 0
+else
+  echo "Failed: output file doesn't exist or is zero length!"
+  exit 1
+fi
