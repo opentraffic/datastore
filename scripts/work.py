38d8c255c61405bed3fa08ef49e1d450a0d2ca82
Grant Heffernan <heffergm@gmail.com>
2017-06-28 10:01:12 -0400

    fix

--- scripts/work.py
+++ scripts/work.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('s3_reporter_bucket', type=str,help='Bucket (e.g. reporter-drop-prod) in which the data we wish to process is located')
     parser.add_argument('s3_datastore_bucket', type=str, help='Bucket (e.g. datastore-output-prod) into which we will place transformed data')
-    parser.add_argument('s3_reporter_keys', type=str, help='S3 object keys which we will operate on, found in the s3_reporter_bucket'
+    parser.add_argument('s3_reporter_keys', type=str, help='S3 object keys which we will operate on, found in the s3_reporter_bucket')
     parser.add_argument('time_bucket', type=int, help='The time bucket')
     parser.add_argument('tile_id', type=int, help='The tile ID')
     args = parser.parse_args()
