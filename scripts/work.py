6a148cead68cec4233d371ab7a3cf99d9d681e6c
Grant Heffernan <heffergm@gmail.com>
2017-06-28 13:30:30 -0400

    Revert "give up on subprocess"
    This reverts commit 028f8cff46848c0b43843dd6f10d4c0e6d37ec50.

--- scripts/work.py
+++ scripts/work.py
@@ -4,7 +4,7 @@ import os
 import sys
 import boto3
 import argparse
-#import subprocess
+import subprocess
 
 def cleanup():
     pass
@@ -30,9 +30,7 @@ def convert(keys_array):
 
     # TODO: error handling?
     sys.stdout.flush()
-    #process = subprocess.run(['datastore-histogram-tile-writer', '--time-bucket', str(args.time_bucket), '--tile', str(args.tile_id), '-f', 'flatbuffer_file', '-o', 'orc_file', file_list], timeout=300, check=True)
-    cmd = 'datastore-histogram-tile-writer --time-bucket ' + str(args.time_bucket) + ' --tile ' + str(args.tile_id) + ' -f flatbuffer_file -o orc_file ' + file_list'
-    process = os.system(cmd)
+    process = subprocess.run(['datastore-histogram-tile-writer', '--time-bucket', str(args.time_bucket), '--tile', str(args.tile_id), '-f', 'flatbuffer_file', '-o', 'orc_file', file_list], timeout=300, check=True)
     print('[INFO] Finished running conversion')
 
 def download(keys_array):
