ca8b94452806c430404f6528bf332bc1e3e521e4
kdiluca <kristen@mapzen.com>
2017-09-01 20:51:58 -0400

    a few more fixes to output ref tiles

--- scripts/make_ref_speeds.py
+++ scripts/make_ref_speeds.py
@@ -20,7 +20,7 @@ except ImportError:
 ### segment0{refspdlist[hour0:avgspd, hour1:avgspd, hour2:avgspd, etc]} -> sort lo to hi, index in to get refspd20:int, refspd40:int, refspd60:int, refspd80:int
 ### segment1{refspdlist[hour0:avgspd, hour1:avgspd, hour2:avgspd, etc]}
 # get the avg speeds for a given segment for each hour (168 hr/day) over 52 weeks
-<<<<<<< HEAD
+
 def createAvgSpeedList(fileNameList):
   #each segment has its own list of speeds, we dont know how many segments for the avg speed list to start with
   speedListPerSegment = []
@@ -36,52 +36,53 @@ def createAvgSpeedList(fileNameList):
     for subtile in spdtile.subtiles:
 
       #make sure that there are enough lists in the list for all segments
-      print 'length of avg speed list per segment ' + str(len(speedListPerSegment))
-      missing = len(speedListPerSegment) - subtile.totalSegments
+      missing = subtile.totalSegments - len(speedListPerSegment)
       if missing > 0:
         speedListPerSegment.extend([ [] for i in range(0, missing) ])
- 
-      print 'total # of segments ' + str(subtile.totalSegments) 
+      #print 'total # created in speedListPerSegment ' + str(len(speedListPerSegment))
+
       for i, speed in enumerate(subtile.speeds):
-        if speedListPerSegment[subtile.startSegmentIndex + i]:
-          if speed is not 0:
-            speedListPerSegment[subtile.startSegmentIndex + i].append(speed)
+        if speed and speed > 0:
+          speedListPerSegment.insert(subtile.startSegmentIndex + i, speed)
+          #print 'index=' + str(i) + ' | speeds=' + str(speed) + '| startSegmentIndex+1= ' + str(subtile.startSegmentIndex + i)
 
-
-    #sort each list of speeds per segment
-    for i, segment in enumerate(speedListPerSegment):
-      print '# of valid average speeds in segment ' + str(i) + ' is ' + str(len(speedListPerSegment))
-      #sort lo to hi
+      speedListPerSegment = filter(None, speedListPerSegment)
+      #sort hi to lo
       speedListPerSegment.sort()
 
   return speedListPerSegment
-  
+
 ###############################################################################
-def createRefSpeedTile(path, fileName, avgspdlist):
+def createRefSpeedTile(path, fileName, speedListPerSegment):
   log.debug('createRefSpeedTiles ###############################################################################')
 
   tile = speedtile_pb2.SpeedTile()
   st = tile.subtiles.add()
-  st.level = #TODO: get from osmlr
-  st.index = #TODO: get from osmlr
+  #st.level = 0 #TODO: get from osmlr
+  #st.index = 415 #TODO: get from osmlr
   st.startSegmentIndex = 0
-  st.totalSegments = len(avgspdlist)
-  st.subtileSegments = len(avgspdlist)
+  st.totalSegments = len(speedListPerSegment)
+  st.subtileSegments = len(speedListPerSegment)
   #time stuff
-  st.rangeStart = #TODO: first second since epoch of first week you have data for
-  st.rangeEnd = #TODO: last second since epoch of last week you have data for
+  #st.rangeStart = #TODO: first second since epoch of first week you have data for
+  #st.rangeEnd = #TODO: last second since epoch of last week you have data for
   st.unitSize = st.rangeEnd - st.rangeStart
   st.entrySize = st.unitSize
   st.description = 'Reference speeds over 1 year from 08.2016 through 07.2017' #TODO: get this from range start and end
 
 
   #for each segment
-  for segment in avgspdlist:
-    size = len(segment)
-    st.referenceSpeed20(segment[round(size * .2)])
-    st.referenceSpeed40(segment[round(size * .2)])
-    st.referenceSpeed60(segment[round(size * .2)])
-    st.referenceSpeed80(segment[round(size * .2)])
+  print 'speedListPerSegment length: ' + str(len(speedListPerSegment))
+  #bucketize avg speeds into 20%, 40%, 60% and 80% reference speed buckets
+  size = len(speedListPerSegment)
+  print speedListPerSegment[int(size * .2)]
+  print speedListPerSegment[int(size * .4)]
+  print speedListPerSegment[int(size * .6)]
+  print speedListPerSegment[int(size * .8)]
+  st.referenceSpeed20.append(speedListPerSegment[int(size * .2)])
+  st.referenceSpeed40.append(speedListPerSegment[int(size * .4)])
+  st.referenceSpeed60.append(speedListPerSegment[int(size * .6)])
+  st.referenceSpeed80.append(speedListPerSegment[int(size * .8)])
 
   #write it out
   with open(path + fileName, 'ab') as f:
@@ -90,24 +91,25 @@ def createRefSpeedTile(path, fileName, avgspdlist):
 #Read in protobuf files from the datastore output in AWS to read in the lengths, speeds & next segment ids and generate the segment speed files in proto output format
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Generate speed tiles', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument('--speedtile-list', type=str, nargs='+', help='A list of the public data extract speed tiles containing the average speeds per segment per hour of day for one week')
+  parser.add_argument('--speedtile-list', type=str, nargs='+', help='A list of the PDE speed tiles containing the average speeds per segment per hour of day for one week')
   parser.add_argument('--ref-tile-path', type=str, help='The public data extract speed tile containing the average speeds per segment per hour of day for one week', required=True)
-  parser.add_argument('--output-prefix', type=str, help='The file name prefix to give to output tiles. The first tile will have no suffix, after that they will be numbered starting at 1. e.g. tile.ref, tile.ref.1, tile.ref.2', default='tile.ref')
+  parser.add_argument('--output-prefix', type=str, help='The file name prefix to give to output tile.')
   parser.add_argument('--verbose', '-v', help='Turn on verbose output i.e. DEBUG level logging', action='store_true')
 
   # parse the arguments
   args = parser.parse_args()
 
   print 'getting avg speeds from list of protobuf speed tile extracts'
-  avgspdlist = createAvgSpeedList(args.speedtile_list)
+  speedListPerSegment = createAvgSpeedList(args.speedtile_list)
   
   #print 'create reference speed tiles for each segment'
-  createRefSpeedTile(args.ref_tile_path, args.output_prefix, avgspdlist)
+  createRefSpeedTile(args.ref_tile_path, args.output_prefix, speedListPerSegment)
 
   if args.verbose:
     log.debug('loop over segments ###############################################################################')
-    for i,speeds in enumerate(segments):
-      log.debug('index=' + str(i) + ' | speeds=' + str(speeds))
+    for i, speed in enumerate(speedListPerSegment):
+      log.debug('index=' + str(i) + ' | speeds=' + str(speed))
+
     log.debug('DONE loop over segments ###############################################################################')
 
   print 'done'
