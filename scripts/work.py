d7cbeefdd73231ecafd12216ad99b0bd76004f62
Kevin Kreiser <kevinkreiser@gmail.com>
2017-09-05 15:36:45 -0400

    show the output of the flatbuffer conversion

--- scripts/work.py
+++ scripts/work.py
@@ -39,7 +39,9 @@ def convert(tile_index, time_bucket, tile_id):
 
     # TODO: no idea if the exception handling works
     try:
-        subprocess.check_output(['datastore-histogram-tile-writer', '-b', str(time_bucket), '-t', str(tile_id), '-v', '-f', fb_out_file, fb_out_file] + glob.glob('*'), timeout=180, universal_newlines=True, stderr=subprocess.STDOUT)
+        output = subprocess.check_output(['datastore-histogram-tile-writer', '-b', str(time_bucket), '-t', str(tile_id), '-f', fb_out_file, fb_out_file] + glob.glob('*'), timeout=180, universal_newlines=True, stderr=subprocess.STDOUT)
+        for line in output.splitlines():
+            print line
     except subprocess.CalledProcessError as tilewriter:
         print('[ERROR] Failed running datastore-histogram-tile-writer:', tilewriter.returncode, tilewriter.output)
         sys.exit([tilewriter.returncode])
