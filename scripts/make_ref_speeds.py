29b9b1c11e13a0375f6323d10aed47e845c4ae40
kdiluca <kristen@mapzen.com>
2017-09-06 10:42:02 -0400

    added in url listing from aws and refactor processing for ref speeds

--- scripts/make_ref_speeds.py
+++ scripts/make_ref_speeds.py
@@ -21,12 +21,12 @@ except ImportError:
 ### segment1{refspdlist[hour0:avgspd, hour1:avgspd, hour2:avgspd, etc]}
 # get the avg speeds for a given segment for each hour (168 hr/day) over 52 weeks
 
-def createAvgSpeedList(fileNameList):
+def createAvgSpeedList(fileNames):
   #each segment has its own list of speeds, we dont know how many segments for the avg speed list to start with
   segments = []
 
   #need to loop thru all of the speed tiles for a given tile id
-  for fileName in fileNameList:
+  for fileName in fileNames:
     #lets load the protobuf speed tile
     spdtile = speedtile_pb2.SpeedTile()
     with open(fileName, 'rb') as f:
@@ -42,9 +42,10 @@ def createAvgSpeedList(fileNameList):
       
       print 'total # created in segments ' + str(subtile.totalSegments)
       entries = subtile.unitSize / subtile.entrySize
+      print '# of entries per segment : ' + str(entries)
       for i, speed in enumerate(subtile.speeds):
         if speed > 0:
-          segments[subtile.startSegmentIndex + (i%entries)].append(speed)
+          segments[subtile.startSegmentIndex + int(math.floor(i/entries))].append(speed)
           #print 'index=' + str(i) + ' | speeds=' + str(speed) + '| startSegmentIndex+i= ' + str(subtile.startSegmentIndex + (i%entries))
 
   #sort each list of speeds per segment
@@ -78,19 +79,87 @@ def createRefSpeedTile(path, fileName, speedListPerSegment):
   #for each segment
   for segment in speedListPerSegment:
     size = len(segment)
-    st.referenceSpeed20.append(segment[int(size * .2)] if size > 0 else 0) #0 here represents no data
-    st.referenceSpeed40.append(segment[int(size * .4)] if size > 0 else 0) #0 here represents no data
-    st.referenceSpeed60.append(segment[int(size * .6)] if size > 0 else 0) #0 here represents no data
-    st.referenceSpeed80.append(segment[int(size * .8)] if size > 0 else 0) #0 here represents no data
+    st.referenceSpeeds20.append(segment[int(size * .2)] if size > 0 else 0) #0 here represents no data
+    st.referenceSpeeds40.append(segment[int(size * .4)] if size > 0 else 0) #0 here represents no data
+    st.referenceSpeeds60.append(segment[int(size * .6)] if size > 0 else 0) #0 here represents no data
+    st.referenceSpeeds80.append(segment[int(size * .8)] if size > 0 else 0) #0 here represents no data
 
-  print str(st.referenceSpeed20)
-  print str(st.referenceSpeed40)
-  print str(st.referenceSpeed60)
-  print str(st.referenceSpeed80)
+  #print str(st.referenceSpeeds20)
+  #print str(st.referenceSpeeds40)
+  #print str(st.referenceSpeeds60)
+  #print str(st.referenceSpeeds80)
 
   #write it out
   with open(path + fileName, 'ab') as f:
     f.write(tile.SerializeToString())
+
+###############################################################################
+    #world bb
+minx_ = -180
+miny_ = -90
+maxx_ = 180
+maxy_ = 90
+
+class BoundingBox(object):
+
+  def __init__(self, min_x, min_y, max_x, max_y):
+     self.minx = min_x
+     self.miny = min_y
+     self.maxx = max_x
+     self.maxy = max_y
+
+class TileHierarchy(object):
+
+  def __init__(self):
+    self.levels = {}
+    # local
+    self.levels[2] = Tiles(BoundingBox(minx_,miny_,maxx_,maxy_),.25)
+    # arterial
+    self.levels[1] = Tiles(BoundingBox(minx_,miny_,maxx_,maxy_),1)
+    # highway
+    self.levels[0] = Tiles(BoundingBox(minx_,miny_,maxx_,maxy_),4)
+
+class Tiles(object):
+
+  def __init__(self, bbox, size):
+     self.bbox = bbox
+     self.tilesize = size
+
+     self.ncolumns = int(math.ceil((self.bbox.maxx - self.bbox.minx) / self.tilesize))
+     self.nrows = int(math.ceil((self.bbox.maxy - self.bbox.miny) / self.tilesize))
+     self.max_tile_id = ((self.ncolumns * self.nrows) - 1)
+
+  def Digits(self, number):
+    digits = 1 if (number < 0) else 0
+    while long(number):
+       number /= 10
+       digits += 1
+    return long(digits)
+
+  # get the File based on tile_id and level
+  def GetFile(self, tile_id, level):
+    max_length = self.Digits(self.max_tile_id)
+
+    remainder = max_length % 3
+    if remainder:
+       max_length += 3 - remainder
+
+    #if it starts with a zero the pow trick doesn't work
+    if level == 0:
+      file_suffix = '{:,}'.format(int(pow(10, max_length)) + tile_id).replace(',', '/')
+      file_suffix += "."
+      file_suffix += "spd.*.gz"
+      file_suffix = "0" + file_suffix[1:]
+      return file_suffix
+
+    #it was something else
+    file_suffix = '{:,}'.format(level * int(pow(10, max_length)) + tile_id).replace(',', '/')
+    file_suffix += "."
+    file_suffix += "spd.*.gz"
+
+    return file_suffix
+
+###############################################################################
 
 #Read in protobuf files from the datastore output in AWS to read in the lengths, speeds & next segment ids and generate the segment speed files in proto output format
 if __name__ == "__main__":
@@ -99,11 +168,34 @@ if __name__ == "__main__":
   parser.add_argument('--ref-tile-path', type=str, help='The public data extract speed tile containing the average speeds per segment per hour of day for one week', required=True)
   parser.add_argument('--output-prefix', type=str, help='The file name prefix to give to output tile.')
   parser.add_argument('--verbose', '-v', help='Turn on verbose output i.e. DEBUG level logging', action='store_true')
+  parser.add_argument('--bucket', type=str, help='AWS bucket location', required=True)
+  parser.add_argument('--year', type=str, help='The year you wish to get', required=True)
+  parser.add_argument('--level', type=int, help='The level to target', required=True)
+  parser.add_argument('--tile-id', type=int, help='The tile id to target', required=True)
+  parser.add_argument('--no-separate-subtiles', help='If present all subtiles will be in the same tile', action='store_true')
 
   # parse the arguments
   args = parser.parse_args()
 
+  tile_hierarchy = TileHierarchy()
+
+  url_prefix = "https://s3.amazonaws.com/" + args.bucket + "/" + args.year + "/"
+
+  weeks_per_year = 52
+  week = 0
+  #urls = []
+  while ( week < weeks_per_year):
+    url = url_prefix + str(week) + "/"
+    file_name = tile_hierarchy.levels[args.level].GetFile(args.tile_id, args.level)
+    url += file_name
+    week += 1
+    #urls.append(url)
+    print url
+
+################################################################################
+
   print 'getting avg speeds from list of protobuf speed tile extracts'
+  #speedListPerSegment = createAvgSpeedList(fileNames)
   speedListPerSegment = createAvgSpeedList(args.speedtile_list)
   
   #print 'create reference speed tiles for each segment'
