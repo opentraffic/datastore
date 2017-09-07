a27399921075d20273e2e992f06b5db8a8600bac
gknisely <greg@mapzen.com>
2017-09-07 13:14:34 -0400

    download the speed tiles from aws and decompress

--- scripts/make_ref_speeds.py
+++ scripts/make_ref_speeds.py
@@ -10,6 +10,8 @@ import botocore
 import shutil
 import gzip
 import logging as log
+from Queue import Queue
+from threading import Thread
 
 try:
   import speedtile_pb2
@@ -27,10 +29,11 @@ maxy_ = 90
 def get_tile_count(filename):
   #lets load the protobuf speed tile
   spdtile = speedtile_pb2.SpeedTile()
-  with open(directory + key, 'rb') as f:
+  with open(filename, 'rb') as f:
     spdtile.ParseFromString(f.read())
-  # 0 based so subtract 1
-  return int(round(spdtile.subtiles[0].totalSegments / (spdtile.subtiles[0].subtileSegments * 1.0))) - 1
+  if spdtile.subtiles[0].totalSegments <= spdtile.subtiles[0].subtileSegments:
+    return 0
+  return int(math.ceil(spdtile.subtiles[0].totalSegments / (spdtile.subtiles[0].subtileSegments * 1.0)))
 
 ###############################################################################
 ### tile 2415
@@ -149,6 +152,7 @@ class Tiles(object):
 
   # get the File based on tile_id and level
   def GetFile(self, tile_id, level):
+
     max_length = self.Digits(self.max_tile_id)
 
     remainder = max_length % 3
@@ -159,16 +163,47 @@ class Tiles(object):
     if level == 0:
       file_suffix = '{:,}'.format(int(pow(10, max_length)) + tile_id).replace(',', '/')
       file_suffix += "."
-      file_suffix += "spd.0"
+      file_suffix += "spd"
       file_suffix = "0" + file_suffix[1:]
       return file_suffix
 
     #it was something else
     file_suffix = '{:,}'.format(level * int(pow(10, max_length)) + tile_id).replace(',', '/')
     file_suffix += "."
-    file_suffix += "spd.0"
-
+    file_suffix += "spd"
     return file_suffix
+
+#this is from:
+#http://code.activestate.com/recipes/577187-python-thread-pool/
+
+class Worker(Thread):
+    """Thread executing tasks from a given tasks queue"""
+    def __init__(self, tasks):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            func, args, kargs = self.tasks.get()
+            try: func(*args, **kargs)
+            except Exception, e: print e
+            self.tasks.task_done()
+
+class ThreadPool:
+    """Pool of threads consuming tasks from a queue"""
+    def __init__(self, num_threads):
+        self.tasks = Queue(num_threads)
+        for _ in range(num_threads): Worker(self.tasks)
+
+    def add_task(self, func, *args, **kargs):
+        """Add a task to the queue"""
+        self.tasks.put((func, args, kargs))
+
+    def wait_completion(self):
+        """Wait for completion of all the tasks in the queue"""
+        self.tasks.join()
 
 ###############################################################################
 
@@ -202,9 +237,24 @@ if __name__ == "__main__":
 
   ################################################################################
   # download the speed tiles from aws and decompress
+  spdFileNames = []
   if not args.local:
-    log.debug('Download the speed tiles from AWS and decompress...')
-    spdFileNames = []
+
+    # work function for our threads.  Downloads and decompresses the speed tile
+    def work(bucket, directory, filename):
+      s3 = boto3.client('s3')
+      try:
+        s3.download_file(bucket, filename + ".gz", directory + filename + ".gz")
+      except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+          print "File not found in bucket! " + filename + ".gz"
+          raise
+      decompressedFile = gzip.GzipFile(directory + filename + ".gz", mode='rb')
+      with open(directory + filename, 'w') as outfile:
+        outfile.write(decompressedFile.read())
+      print('[INFO] downloaded and decompressed file: ' + filename + '.gz from s3 bucket: ' + bucket)
+
+    #our work dir.  deleted everytime if exists
     directory = "ref_working_dir/"
     shutil.rmtree(directory, ignore_errors=True)
     tile_hierarchy = TileHierarchy()
@@ -212,31 +262,58 @@ if __name__ == "__main__":
 
     s3 = boto3.client('s3')
 
+    #get the first tile *.0.gz so that we can determine the number of subtiles we have
+    # if they are separated into subtiles
+    print('[INFO] starting download from s3 bucket: ' + args.bucket)
     weeks_per_year = 52
     week = 0
+    # for every week in the year
     while ( week < weeks_per_year):
       key = key_prefix + str(week) + "/"
       file_name = tile_hierarchy.levels[args.level].GetFile(args.tile_id, args.level)
       key += file_name
       try:
-        file_path = os.path.dirname(key + ".gz" )
+        file_path = os.path.dirname(key + ".0.gz" )
+        # create the directory for the tiles
         if not os.path.exists(directory + file_path):
           try:
             os.makedirs(directory + file_path)
           except OSError as e:
             if e.errno != errno.EEXIST:
               raise
-        with open(directory + key + ".gz" , "wb") as f:
-          s3.download_fileobj(args.bucket, key + ".gz", f)
+        #download the file
+        s3.download_file(args.bucket, key + ".0.gz", (directory + key + ".0.gz"))
       except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] != "404":
           raise
-      decompressedFile = gzip.GzipFile(directory + key + ".gz", mode='rb')
-      with open(directory + key , 'w') as outfile:
+        else:
+          week += 1
+          continue
+      print('[INFO] downloaded and decompressed file: ' + (key + ".0.gz") + ' from s3 bucket: ' + args.bucket)
+      #decompress the file
+      decompressedFile = gzip.GzipFile(directory + key + ".0.gz", mode='rb')
+      with open(directory + key + ".0", 'w') as outfile:
         outfile.write(decompressedFile.read())
 
-      spdFileNames.append(outfile.name)
+      #append the file name
+      spdFileNames.append(os.path.abspath(outfile.name))
+
+      #if everything is in one tile, then we are done.
+      if not args.no_separate_subtiles:
+        subtile_suffix = get_tile_count(directory + key + ".0")
+
+        # create a thread pool based on the subtile count, download, and decompress them
+        if subtile_suffix != 0:
+          p=ThreadPool(subtile_suffix)
+          i = 1;
+          while ( i < subtile_suffix):
+            p.add_task(work, args.bucket, directory, key + "." + str(i))
+            spdFileNames.append(os.path.abspath(directory + key + "." + str(i)))
+            i += 1
+          p.wait_completion()
+
       week += 1
+
   ################################################################################
 
   print 'getting avg speeds from list of protobuf speed tile extracts'
