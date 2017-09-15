3cd048bab49238ba4b4b4e6e032d12605e1d3aea
Kevin Kreiser <kevinkreiser@gmail.com>
2017-09-15 17:13:03 -0400

    use a prefix to get all the tiles you can, then make sure to delte them and that they are actually deleted

--- scripts/work.py
+++ scripts/work.py
@@ -9,6 +9,7 @@ import boto3
 import logging
 import threading
 import math
+import time
 from botocore.exceptions import ClientError
 
 logger = logging.getLogger('make_histograms')
@@ -17,128 +18,158 @@ handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s'))
 logger.addHandler(handler)
 
-def get_time_key(time_bucket, tile_level, tile_index):
-    # path key: year/month/day/hour/tile_level/tile_index
-    to_time = time.gmtime(time_bucket * 3600)
-    time_key = str(to_time[0]) + '/' + str(to_time[1]) + '/' + str(to_time[2]) + '/' + str(to_time[3]) + '/' + str(tile_level) + '/' + str(tile_index)
-    return time_key
+def parse_prefix(prefix):
+  # input prefix: epochsecond_epochsecond/tile_level/tile_index
+  # dest key: year/month/day/hour/tile_level/tile_index
+  parts = prefix.split('/')
+  epoch = int(parts[0].split('_')[0])
+  to_time = time.gmtime(epoch)
+  dest = str(to_time[0]) + '/' + str(to_time[1]) + '/' + str(to_time[2]) + '/' + str(to_time[3]) + '/' + parts[1] + '/' + parts[2] + '.fb'
+  #time_bucket, tile_id, destination key
+  return int(epoch / 3600), (int(parts[2]) << 3) | int(parts[1]), dest
 
-def upload(time_key, s3_datastore_bucket):
-    logger.info('uploading data to bucket: ' + s3_datastore_bucket)
-    s3_client = boto3.client('s3')
+def convert(time_bucket, tile_id, dest_key):
+  logger.info('running conversion process')
+  fb_out_file = dest_key.split('/')[-1]
+  
+  # TODO: no idea if the exception handling works
+  try:
+    output = subprocess.check_output(['datastore-histogram-tile-writer', '-b', str(time_bucket), '-t', str(tile_id), '-f', fb_out_file] + glob.glob('*'), timeout=180, universal_newlines=True, stderr=subprocess.STDOUT)
+    for line in output.splitlines():
+      logger.info(line)
+  except subprocess.CalledProcessError as tilewriter:
+    logger.error('Failed running datastore-histogram-tile-writer: ' + str(tilewriter.returncode) + ' ' + str(tilewriter.output))
+    sys.exit(tilewriter.returncode)
+  logger.info('Finished running conversion')
 
-    uploads = ['.fb']
-    for file_extension in uploads:
-        key = time_key + file_extension
+def upload(dest_key, s3_datastore_bucket):
+  s3_client = boto3.client('s3')
+  with open(dest_key.split('/')[-1], 'rb') as data:
+    logger.info('Uploading to ' + s3_datastore_bucket + ' ' + dest_key)
+    s3_client.put_object(Bucket=s3_datastore_bucket, ContentType='binary/octet-stream', Body=data, Key=dest_key)
 
-        data = open(time_key.rsplit('/', 1)[-1] + file_extension, 'rb')
-        s3_client.put_object(
-            Bucket=s3_datastore_bucket,
-            ContentType='binary/octet-stream',
-            Body=data,
-            Key=key)
-        data.close()
+def delete_keys(keys, s3_reporter_bucket):
+  secs = 2
+  s3_client = boto3.client('s3')
+  for key in keys:
+    logger.info('Deleting from ' + s3_reporter_bucket + ' ' + key)
+    while True:
+      try:
+        s3_client.delete_object(Bucket=s3_reporter_bucket, Key=key)
+        s3_client.head_object(Bucket=s3_reporter_bucket, Key=key)
+      except Exception as e:
+        if e.response['ResponseMetadata']['HTTPStatusCode'] == 404:
+          break
+        logger.error('Failed to delete ' + key + ': ' + str(d))
+        logger.info('Sleeping %d seconds until next try' % secs)
+        time.sleep(secs)
+        secs *= secs
 
-def convert(tile_index, time_bucket, tile_id):
-    logger.info('running conversion process')
-    sys.stdout.flush()
-
-    fb_out_file = str(tile_index) + '.fb'
-
-    # TODO: no idea if the exception handling works
-    try:
-        output = subprocess.check_output(['datastore-histogram-tile-writer', '-b', str(time_bucket), '-t', str(tile_id), '-f', fb_out_file] + glob.glob('*'), timeout=180, universal_newlines=True, stderr=subprocess.STDOUT)
-        for line in output.splitlines():
-            logger.info(line)
-    except subprocess.CalledProcessError as tilewriter:
-        logger.error('Failed running datastore-histogram-tile-writer: ' + str(tilewriter.returncode) + ' ' + str(tilewriter.output))
-        sys.exit(tilewriter.returncode)
-    logger.info('Finished running conversion')
+def delete(keys, s3_reporter_bucket):
+  # delete the files
+  chunks = split(keys, 10)
+  threads = []
+  for chunk in chunks:
+    threads.append(threading.Thread(target=delete_keys, args=(chunk, s3_reporter_bucket)))
+    threads[-1].start()
+  for t in threads:
+    t.join()
 
 def get_files(keys, s3_reporter_bucket, s3_datastore_bucket):
-    for key in keys:
-        object_id = key.rsplit('/', 1)[-1]
-        session = boto3.session.Session()
-        s3_resource = session.resource('s3')
-        logger.info('downloading ' + object_id + ' from s3 bucket: ' + s3_reporter_bucket)
-        try:
-            if key.endswith('.fb'):
-                logger.info('downloading ' + key + ' as ' + object_id + ' from s3 bucket: ' + s3_datastore_bucket)
-                s3_resource.Object(s3_datastore_bucket, key).download_file(object_id)
-            else:
-                logger.info('downloading ' + key + ' as ' + object_id + ' from s3 bucket: ' + s3_reporter_bucket)
-                s3_resource.Object(s3_reporter_bucket, key).download_file(object_id)
-        except Exception as e:
-            logger.error('Failed to download key: %s' % e)
+  for key in keys:
+    object_id = key.rsplit('/', 1)[-1]
+    session = boto3.session.Session()
+    s3_resource = session.resource('s3')
+    logger.info('downloading ' + object_id + ' from s3 bucket: ' + s3_reporter_bucket)
+    try:
+      if key.endswith('.fb'):
+        logger.info('downloading ' + key + ' as ' + object_id + ' from s3 bucket: ' + s3_datastore_bucket)
+        s3_resource.Object(s3_datastore_bucket, key).download_file(object_id)
+      else:
+        logger.info('downloading ' + key + ' as ' + object_id + ' from s3 bucket: ' + s3_reporter_bucket)
+        s3_resource.Object(s3_reporter_bucket, key).download_file(object_id)
+    except Exception as e:
+      logger.error('Failed to download key: %s' % e)
 
 def split(l, n):
-    size = int(math.ceil(len(l)/float(n)))
-    cutoff = len(l) % n
-    result = []
-    pos = 0
-    for i in range(0, n):
-        end = pos + size if i < cutoff else pos + size - 1
-        result.append(l[pos:end])
-        pos = end
-    return result
+  size = int(math.ceil(len(l)/float(n)))
+  cutoff = len(l) % n
+  result = []
+  pos = 0
+  for i in range(0, n):
+    end = pos + size if i < cutoff else pos + size - 1
+    result.append(l[pos:end])
+    pos = end
+  return result
 
-def download_data(prefixes_array, s3_reporter_bucket, s3_datastore_bucket, time_key):
-    client = boto3.client('s3')
-    s3_resource = boto3.resource('s3')
-    
-    # get the keys
-    keys = []
-    for prefix in prefixes_array:
-        token = None
-        first = True
-        while first or token:
-            if token:
-                objects = client.list_objects_v2(Bucket=s3_reporter_bucket, Delimiter='/', Prefix=prefix, ContinuationToken=token)
-            else:
-                objects = client.list_objects_v2(Bucket=s3_reporter_bucket, Delimiter='/', Prefix=prefix)
-            keys.extend([ o['Key'] for o in objects['Contents']])
-            token = objects.get('NextContinuationToken')
-            first = False
-    # add the key for the existing file
-    keys.append(time_key + '.fb')
+def get_prefixes_keys(client, bucket, prefixes):
+  keys = []
+  pres = []
+  for prefix in prefixes:
+    token = None
+    first = True
+    while first or token:
+      if token:
+        objects = client.list_objects_v2(Bucket=bucket, Delimiter='/', Prefix=prefix, ContinuationToken=token)
+      else:
+        objects = client.list_objects_v2(Bucket=bucket, Delimiter='/', Prefix=prefix)
+      if 'Contents' in objects:
+        keys.extend([ o['Key'] for o in objects['Contents'] ])
+      if 'CommonPrefixes' in objects:
+        pres.extend([ o['Prefix'] for o in objects['CommonPrefixes'] ])
+      token = objects.get('NextContinuationToken')
+      first = False
+  return pres, keys
 
-    # download the files
-    keys = split(keys, 10)
-    threads = []
-    for chunk in keys:
-        threads.append(threading.Thread(target=get_files, args=(chunk, s3_reporter_bucket, s3_datastore_bucket)))
-        threads[-1].start()
-    for t in threads:
-        t.join()
+def download_data(prefix, s3_reporter_bucket, s3_datastore_bucket, dest_key):
+  client = boto3.client('s3')
+
+  # get the keys for the files in this tile
+  _, keys = get_prefixes_keys(client, s3_reporter_bucket, [prefix])
+ 
+  # add the key for the existing file
+  keys.append(dest_key)
+
+  # download the files
+  chunks = split(keys, 10)
+  threads = []
+  for chunk in chunks:
+    threads.append(threading.Thread(target=get_files, args=(chunk, s3_reporter_bucket, s3_datastore_bucket)))
+    threads[-1].start()
+  for t in threads:
+    t.join()
+
+  return keys[:-1]
 
 if __name__ == "__main__":
-    # build args
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--s3-reporter-bucket', type=str, help='Bucket (e.g. reporter-work-prod) in which the data we wish to process is located')
-    parser.add_argument('--s3-datastore-bucket', type=str, help='Bucket (e.g. datastore-output-prod) into which we will place transformed data')
-    parser.add_argument('--s3-reporter-keys', type=str, help='S3 object keys which we will operate on, found in the s3_reporter_bucket')
-    parser.add_argument('--time-bucket', type=int, help='The time bucket')
-    parser.add_argument('--tile-id', type=int, help='The tile ID')
-    parser.add_argument('--tile-level', type=int, help='The tile level')
-    parser.add_argument('--tile-index', type=int, help='The tile index')
-    args = parser.parse_args()
+  # build args
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--s3-reporter-bucket', type=str, help='Bucket (e.g. reporter-work-prod) in which the data we wish to process is located')
+  parser.add_argument('--s3-datastore-bucket', type=str, help='Bucket (e.g. datastore-output-prod) into which we will place transformed data')
+  parser.add_argument('--s3-reporter-prefix', type=str, help='S3 prefix under which tiles will be found, should look like epochsecond_epochsecond/level/tile_index')
+  args = parser.parse_args()
 
-    logger.info('reporter input bucket: ' + args.s3_reporter_bucket)
-    logger.info('datastore output bucket: ' + args.s3_datastore_bucket)
-    logger.info('time bucket: ' + str(args.time_bucket))
-    logger.info('tile id: ' + str(args.tile_id))
-    logger.info('tile level: ' + str(args.tile_level))
-    logger.info('tile index: ' + str(args.tile_index))
+  logger.info('reporter input bucket: ' + args.s3_reporter_bucket)
+  logger.info('datastore output bucket: ' + args.s3_datastore_bucket)
+  logger.info('reporter prefix: ' + str(args.s3_reporter_prefix))
 
-    # do work
-    time_key = get_time_key(args.time_bucket, args.tile_level, args.tile_index)
+  #where will this thing end up
+  time_bucket, tile_id, dest_key = parse_prefix(args.s3_reporter_prefix)
 
-    download_data(
-        args.s3_reporter_keys.split(','),
-        args.s3_reporter_bucket,
-        args.s3_datastore_bucket,
-        time_key)
-    convert(args.tile_index, args.time_bucket, args.tile_id)
-    upload(time_key, args.s3_datastore_bucket)
+  #go download all the tiles
+  keys = download_data(args.s3_reporter_prefix, args.s3_reporter_bucket, args.s3_datastore_bucket, dest_key)
 
-    logger.info('run complete')
+  if not keys:
+    logger.error('Prefix was empty!')
+    sys.exit(1)
+
+  #turn the downloaded files into
+  convert(time_bucket, tile_id, dest_key)
+
+  #upload the finished product
+  upload(dest_key, args.s3_datastore_bucket)
+
+  #delete the input data
+  delete(keys, args.s3_reporter_bucket)
+
+  logger.info('run complete')
